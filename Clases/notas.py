9dff52195a0d1cc7078dd6257e8fa3f85b1ea0fb
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-19 09:45:26 +0200

    creado el bucle para sacar la suma de las notas

--- Clases/notas.py
+++ Clases/notas.py
@@ -1,1 +1,10 @@
+import matplotlib
+
+
+import matplotlib.pyplot
 Notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
+suma_notas = 0
+for i in range (len(Notas)):
+    nota = Notas.pop(0)
+    suma_notas += nota
+print(suma_notas)
