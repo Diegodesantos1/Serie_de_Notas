666ce443c9594f6d7986b916a08e1574327962fc
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-19 18:23:09 +0200

    mejorado el init y cambios en los prints

--- Clases/notas.py
+++ Clases/notas.py
@@ -2,23 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Serie_notas:
-    global notas
-    notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
-    def __init__ (self, notas, suma_notas):
+    def __init__ (self, notas, suma_notas, media, moda, dato_max, dato_min):
         self.notas = notas
         self.suma_notas = suma_notas
+        self.media = media
+        self.moda = moda
+        self.dato_max = dato_max
+        self.dato_min = dato_min
     def media():
+        notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
         suma_notas = 0
         for i in range (len(notas)):
             nota = notas.pop(0)
             suma_notas += nota
         media = suma_notas / 20
-        print(f"La tendencia central de las notas es {media}")
+        print(f"La media de las notas es {media}")
     def moda():
         notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
         notas.sort() ; print(notas)
         moda = max(set(notas), key = notas.count)
-        print(f"la moda es {moda}")
+        print(f"La tendencia central de las notas es {moda}")
     def dispersion():
         x = np.random.randint (1,16, size= 16)
         y = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
