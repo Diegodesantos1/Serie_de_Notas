d61dc9ad72427311a05ec19f6f6521b13eeb015c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-23 16:06:05 +0200

    añadidos los archivos dados

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import pandas as pnd
+import Clases_Rubén.JMPEstadisticas as jmp
+import numpy as np
+
 from Clases import notas
 if __name__ == '__main__':
-    eleccion = int(input(("¿Qué versión quieres usar? \n 1: La creada por mi \n 2: La dada por Rubén")))
+    eleccion = int(input(("¿Qué versión quieres usar? \n 1: La creada por mi \n 2: La dada por Rubén\n")))
     if eleccion == 1:
         notas.iniciar()
     elif eleccion ==2:
-        pass
+        observaciones = pnd.DataFrame({'NOTAS':np.array([3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16])})
+        stats = jmp.JMPEstadisticas(observaciones['NOTAS'])
+        stats.analisisCaracteristica()
