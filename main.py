6ba3048209255c0ab990fd80386137313dd06c47
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-23 15:58:28 +0200

    arreglado el main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from Clases import notas
 if __name__ == '__main__':
-    notas.iniciar()
+    eleccion = int(input(("¿Qué versión quieres usar? \n 1: La creada por mi \n 2: La dada por Rubén")))
+    if eleccion == 1:
+        notas.iniciar()
+    elif eleccion ==2:
+        pass
