77083bf2e6987053ac80bf6c2d058ce0848d8fd4
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-26 14:47:16 +0200

    ya casi funciona

--- Clases/notas.py
+++ Clases/notas.py
@@ -1,40 +1,43 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt ; import pandas as pnd
 
 class Serie_notas:
+    global datos , lista_notas
+    datos = pnd.read_csv("Student_grade.csv", header=0 , sep =",")
+    lista_notas = list(datos["Notas"])
     def __init__ (self, notas, suma_notas, media, moda, dato_max, dato_min, x):
         self.notas = notas
         self.suma_notas = suma_notas ; self.media = media
         self.moda = moda ; self.dato_max = dato_max
         self.dato_min = dato_min ; self.x = x
     def calcular_media():
-        notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
-        suma_notas = 0
-        for i in range (len(notas)):
-            nota = notas.pop(0)
+        suma_notas = 0 ; total = len(lista_notas)
+        for i in range (len(lista_notas)):
+            nota = lista_notas.pop(0)
             suma_notas += nota
-        media = suma_notas / 20
+        media = suma_notas / total
         print(f"\nLa media de las notas es {media}\n")
     def calcular_moda():
-        notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
-        notas.sort() ; moda = max(set(notas), key = notas.count)
+        datos = pnd.read_csv("Student_grade.csv", header=0 , sep =",")
+        lista_notas = list(datos["Notas"])
+        lista_notas.sort() ; moda = max(set(lista_notas), key = lista_notas.count)
         print(f"\nLa tendencia central de las notas es {moda}\n")
     def datos_aberrantes():
-        notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]
-        dato_max = max(notas) ; dato_min = min(notas)
+        datos = pnd.read_csv("Student_grade.csv", header=0 , sep =",")
+        lista_notas = list(datos["Notas"])
+        dato_max = max(lista_notas) ; dato_min = min(lista_notas)
         print(f"\nLos datos aberrantes son {dato_max} and {dato_min}\n")
     def graficos():
-        notas = [3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16] ; notas.sort()
-        y = notas
-        x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-        colores = ["r", "b", "b", "b", "g", "g", "g", "g", "g", "g", "b", "b", "b", "b", "b", "r"]
+        datos = pnd.read_csv("Student_grade.csv", header=0 , sep =",")
+        lista_notas = list(datos["Notas"]) ; lista_notas.sort()
+        x = [1,2,3,4,5,6,7,8,9,10]
         plt.subplot(2, 2, 1)
-        plt.scatter(x,y ,color = colores)
+        plt.scatter(x,lista_notas ,color = "green")
         plt.title("Gráfico de dispersión")
         plt.subplot(2, 2, 2)
-        plt.boxplot([[3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]])
+        plt.boxplot(lista_notas)
         plt.title("Diagrama de cajas")
         plt.subplot(2,2,3)
-        plt.violinplot([[3,19,10,15,14,12,9,8,11,12,11,12,13,11,14,16]])
+        plt.violinplot(lista_notas)
         plt.title("Diagrama de violín")
         plt.show()
 
